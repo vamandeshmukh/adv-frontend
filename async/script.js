183be2f4e25c4f99f04b551643d24f276da12022
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-03-13 17:22:42 +0530

    js-fundas

--- async/script.js
+++ async/script.js
@@ -1,5 +1,8 @@
 console.log('Start');
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function 
+// https://www.w3schools.com/js/js_async.asp
+
 // ===================
 // problem of async js 
 // ===================
