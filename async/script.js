2b11865e722f545b8479c6f23e06fb1aa9205065
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-03-13 14:22:53 +0530

    js-fundas

--- async/script.js
+++ async/script.js
@@ -36,18 +36,29 @@ console.log('Start');
 
 const getFun = () => {
 
+    let isDataAvailable = true; // false 
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve({ text: 'Hello world!' });
+            if (isDataAvailable)
+                resolve({ text: 'Hello world!' });
+            else
+                reject({ message: 'Something is wrong!' });
         }, 2000);
     });
 };
 
+// syntax 
+// functionCall().then(() => { }).catch(() => { });
+
 getFun()
     .then((response) => {
         console.log(response.text);
+    })
+    .catch((error) => {
+        console.log(error.message);
     });
 
 
- 
+
+
 
