e8711ece0c5ca0b9ca71f75d395715a6bb63f5df
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-03-13 12:33:52 +0530

    js-fundas

--- async/script.js
+++ async/script.js
@@ -1,13 +1,16 @@
-console.log('async JS');
+console.log('Start');
+
+const getFun = () => {
+    setTimeout(() => {
+        return { text: 'Hello wold!' };
+    }, 2000);
+};
+const fun = getFun();
+console.log(fun.text);
 
 
 
-
-
-
-
-
-
+// start  hello world
 
 
 
