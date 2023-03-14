42abca8d0c671320f189b787882d4152da54ea48
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-03-14 14:52:01 +0530

    js-fundas

--- jQuery/01-jquery-dom-traversal.js
+++ jQuery/01-jquery-dom-traversal.js
@@ -10,10 +10,9 @@ console.log('DOM Traversal with jQuery');
 
 
 $(document).ready(function () {
-    $('#phoneList').click(function () {
-        // $(this).parents().css('color', 'blue');
-        $(this).parent().css('color', 'blue');
-        // $(this).parentsUntil($('#productList')).css('color', 'blue');
+    $('#phones').click(function () {
+        // your code 
+
     });
 });
 
