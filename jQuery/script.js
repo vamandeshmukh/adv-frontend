21dcd59cdd0c6e195b5e476333c5652f47809fac
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-03-14 12:41:23 +0530

    js-fundas

--- jQuery/script.js
+++ jQuery/script.js
@@ -1,8 +1,27 @@
 console.log('jQuery');
 
 $(document).ready(function () {
-    $("p").click(function () {
+    $("#p2").click(function () {
+        console.log('clicked');
         $(this).hide();
     });
 });
+
+
+$(document).ready(function () {
+    $("#p3").hover(function () {
+        $(this).css('color', 'blue');
+    }, function () {
+        $(this).css('color', 'yellow');
+    });
+});
+
+
+
+
+
+
+
+
+
 
