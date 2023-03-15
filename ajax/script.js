27d1ded0ca17afec2a66b2c055642bf178e8ce71
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-03-15 14:28:17 +0530

    js-fundas

--- ajax/script.js
+++ ajax/script.js
@@ -1,19 +1,37 @@
+console.log('jQuery AJAX Demos');
+
+
+$(document).ready(function () {
+    $("button").click(function () {
+        $.get('https://jsonplaceholder.typicode.com/users/2', function (data, status) {
+            console.log(data);
+        });
+    });
+})
+
+
+
+
+
+
 // https://www.w3schools.com/js/js_ajax_http.asp
 
-console.log('ajax demos');
+// console.log('ajax demos');
+
+// const getData = () => {
+
+//     const http = new XMLHttpRequest(); // xhr
+//     http.onload = function () {
+//         let response = this.responseText;
+//         document.getElementById('userData').innerHTML = response;
+//         console.log(response);
+//     };
+//     http.open('GET', 'https://jsonplaceholder.typicode.com/users/101');
+//     // http.open('POST', 'https://jsonplaceholder.typicode.com/users/4', { a: 'b' });
+//     http.send();
+
+// };
 
 
-const getData = () => {
 
-    const http = new XMLHttpRequest();
 
-    http.onload = function () {
-        let response = this.responseText;
-        document.getElementById('userData').innerHTML = response;
-        console.log(response); 
-    };
-
-    http.open('GET', 'https://jsonplaceholder.typicode.com/users/4');
-    http.send();
-
-};
