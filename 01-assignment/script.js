51a4c8200e9da534646d06e3002e7d4333b5ccbc
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-03-15 16:26:53 +0530

    js-fundas

---
+++ 01-assignment/script.js
@@ -0,0 +1,12 @@
+/*
+assignment: 
+using HTML, JS, jQuery, AJAX create - 
+login form 
+register form 
+
+with these APIs - 
+GET - https://jsonplaceholder.typicode.com/users/ 
+POST - https://jsonplaceholder.typicode.com/users/
+
+
+*/
