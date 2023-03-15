5b92fb49840edca48b9673d462a0531ce1751b1d
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-03-15 10:53:01 +0530

    js-fundas

--- ajax/script.js
+++ ajax/script.js
@@ -1,2 +1,17 @@
 console.log('ajax demos');
 
+
+const getData = () => {
+
+    const http = new XMLHttpRequest();
+
+    http.onload = function () {
+        document.getElementById('userData').innerHTML = this.responseText;
+    };
+    
+    http.open('GET', 'https://jsonplaceholder.typicode.com/users/2');
+    http.send();
+
+
+
+};
