37741b6e33b94cb2f41955d344c7a74256120887
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-03-16 12:31:41 +0530

    js-fundas

--- validation/02-regex.js
+++ validation/02-regex.js
@@ -5,27 +5,29 @@
 // const pattern = /^a$/;
 // const pattern = /^[a-zA-Z0-9@#]{2}$/;
 // const pattern = /^[6-9]{1}[0-9]{9}$/;
-const pattern = /^[6-9]{1}[\d]{9}$/;
+// const pattern = /^[6-9]{1}[\d]{9}$/;
 
-const inputValue = '9870986543';
+// const panCardPattern = /^$/; // do this 
+// const vehicleNumberPattern = /^[\d]{1,2}$/; // do this 
 
-const result = pattern.test(inputValue);
+// const inputValue = '321';
 
-console.log(result);
+// const result = vehicleNumberPattern.test(inputValue);
+
+// console.log(result);
 
 
 
 // const url = 'https://jsonplaceholder.typicode.com/users/';
 
-// // login
-// $(document).ready(function () {
-//     $("#loginForm").submit(function () {
-//         console.log('submitted');
-//         $("#userdata").text($("#username").val());
-//         $('#loginForm').trigger("reset");
-//         const pattern = /^[v]{1}$/;
-//     });
-// });
+// login
+$(document).ready(function () {
+    $("#loginForm").submit(function () {
+        console.log('submitted');
+        $("#userdata").text($("#username").val());
+        $('#loginForm').trigger("reset");
+    });
+});
 
 
 
