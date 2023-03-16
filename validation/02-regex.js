ad3a7c90784f4343e9147b08a96c7e6be0c3b942
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-03-16 11:51:14 +0530

    js-fundas

--- validation/02-regex.js
+++ validation/02-regex.js
@@ -1,13 +1,24 @@
-const url = 'https://jsonplaceholder.typicode.com/users/';
 
-// login
-$(document).ready(function () {
-    $("#loginForm").submit(function () {
-        console.log('submitted');
-        $("#userdata").text($("#username").val());
-        $('#loginForm').trigger("reset");
-    });
-});
+// JavaScript RegEx 
+
+const pattern = /a/;
+const result = pattern.test('aa');
+console.log(result);
+
+
+
+// const url = 'https://jsonplaceholder.typicode.com/users/';
+
+// // login
+// $(document).ready(function () {
+//     $("#loginForm").submit(function () {
+//         console.log('submitted');
+//         $("#userdata").text($("#username").val());
+//         $('#loginForm').trigger("reset");
+//         const pattern = /^[v]{1}$/;
+//     });
+// });
+
 
 
 
